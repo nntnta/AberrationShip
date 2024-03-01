7806a7a5e5d920af0309ff5a4c5054f158d4aeb2
Nont Nonta <nontnta@gmail.com>
2024-03-02 00:00:07 +0700

    1

---
+++ player/ship.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+
+const SPEED = 14000
+
+func _ready():
+	$AnimationPlayer.play('idle')
+
+func _physics_process(delta):
+	# Add the gravity.
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_vector("a", "d", "w", "s")
+	if direction:
+		velocity.x = direction.x * SPEED * delta
+		velocity.y = direction.y * SPEED * delta
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+
+	move_and_slide()
